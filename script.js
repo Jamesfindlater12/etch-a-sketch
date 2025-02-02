2393f5a5a09e45907560acd90ab254b4b4b682c3
jamesfindlater12 <jamesfindlater11@gmail.com>
2025-02-02 16:54:23 +0000

    Game completed

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
     const body = document.querySelector('body');
     const container = document.querySelector('#container');
+    const gridContainer = document.createElement('div');
     const buttonContainer = document.createElement('div');
     const sizeButton = document.createElement('button');
-
+    
+    container.appendChild(gridContainer);
     body.appendChild(buttonContainer);
     buttonContainer.appendChild(sizeButton);
 
-    container.classList.add('container-box');
+    gridContainer.classList.add('container-box');
 
     for (let i = 0; i < (16*16); i++) {
         const box = document.createElement('div');
         box.classList.add('small-box');
-        container.appendChild(box);
+        gridContainer.appendChild(box);
+
+        box.addEventListener('mouseover', () => {
+            box.style.background = "blue";        
+        })
     };
 
     sizeButton.textContent = 'Change Size';
@@ -22,14 +28,28 @@
     const containerBoxClass = document.querySelector('.container-box');
 
     sizeButton.addEventListener("click", function() {
-        const sizeChangeChoice = prompt("Choose grid size between 200 and 750");
+        const sizeChangeChoice = prompt("Choose grid size between 0 and 100");
         sizeChange = parseInt(sizeChangeChoice);   
-        if (!isNaN(sizeChange) && (sizeChange >= 200) && (sizeChange <= 750)) {
-            containerBoxClass.style.height = `${sizeChange}px`;
-            containerBoxClass.style.width = `${sizeChange}px`;
+        
+
+        if (!isNaN(sizeChange) && (sizeChange >= 10) && (sizeChange <= 100)) {
+            gridContainer.innerHTML = "";
+            for (let i = 0; i < (sizeChange*sizeChange); i++) {
+                
+                const box = document.createElement('div');
+                box.classList.add('small-box');
+                box.style.width = `calc(100% / ${sizeChange})`;
+                box.style.height = `calc(100% / ${sizeChange})`;
+                gridContainer.appendChild(box);
+        
+                box.addEventListener('mouseover', () => {
+                    box.style.background = "blue";        
+                })
+            };
             console.log(`Size changed to ${sizeChange}`);
         } else {
-            alert('Please enter a number between 200 and 750');
+            alert('Please enter a number between 10 and 100');
+        
         };
         
     });
